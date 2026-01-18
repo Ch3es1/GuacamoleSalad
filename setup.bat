83b8282479bbf41743648ca52394acc6c161e5ad
Cameron Paul Swart <ieatnickles69@gmail.com>
2026-01-17 20:18:29 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,9 +1,8 @@
 @echo off
 del README.md
 
-curl -L https://github.com/Ch3es1/GuacamoleSalad/releases/download/ReleaseDownload/GuacamoleSalad-main.zip -o "main.zip"
-
 start /b "" cmd /c del "%~f0"&exit /b
+
 
 
 
